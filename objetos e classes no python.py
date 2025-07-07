07a5a2287321898bda0664bfa87a001d1d34fa03
Rubens Gabriel <rugabriel.gabriel@gmail.com>
2025-07-07 16:14:16 -0300

    Mias alguns comandos e conceitos

--- objetos e classes no python.py
+++ objetos e classes no python.py
@@ -1,21 +1,46 @@
 l = [1, 2, 3]
-print(type(l))
+#print(type(l))
 l.append(4)
 string_ = 'str'
-print(type(string_))
+#print(type(string_))
 
 class Exemplo():
     pass
 
 x = Exemplo()
 
-print(type(x))
+#print(type(x))
 
 class Dog:
-    def __init__(self):
+    def __init__(self, raca):
+        self.raca = raca
         self.idade = 10
+        #print(f'{raca} criado')
+    
+    def envelhecer(self):
+        self.idade += 1
+        return self.idade
 
-dog = Dog()
-dog2 = Dog()
-print(dog.idade)
-print(dog2.idade)
+dog = Dog('Labrador')
+dog2 = Dog('Huskie')
+dog.idade = 11
+dog.envelhecer()
+#print(dog.idade)
+#print(dog.raca)
+#print(dog2.idade)
+#print(dog2.raca)
+
+class circle:
+    def __init__(self, raio=1):
+        self.raio = raio
+    
+    def calcula_area(self):
+        return self.raio * self.raio * 3.14
+    
+    def retorna_raio(self):
+        return self.raio
+
+c1 = circle()
+c2 = circle(2)
+print(c1.retorna_raio())
+print(c2.retorna_raio())
